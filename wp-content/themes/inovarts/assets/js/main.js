15d6ee8516113e3e97f4c437606a1fb0352b7376
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-12 23:42:55 -0300

    add script slider

--- wp-content/themes/inovarts/assets/js/main.js
+++ wp-content/themes/inovarts/assets/js/main.js
@@ -1,16 +1,31 @@
-function menu() {
-    var menu = document.getElementsByClassName('floater-menu')[0]
-    menu.onclick = function() {
-        menu.setAttribute('style', 
-        'width: 100vw; height: 100vh; transition-duration: .8s; border-radius: 0; background-color: #707070; cursor:default;')
-    }
-}
-function slider() {
-    var img_1 = document.getElementById('img-1')
-    var img_2 = document.getElementById('img-2')
-    var img_3 = document.getElementById('img-3')
-}
 window.onload = function() {
+    // GLOBAL VAR
+    var doc = document
+
+    // FUNCTIONS DECLARATION
+    function menu() {
+        var menu = doc.getElementsByClassName('floater-menu')[0]
+        menu.onclick = function() {
+            menu.setAttribute('style', 
+            'width: 100vw; height: 100vh; transition-duration: .8s; border-radius: 0; background-color: #707070; cursor:default;')
+        }
+    }
+
+    function slider() {
+        var arrowL = doc.createElement('div')
+        var arrowR = doc.createElement('div')
+        var slide = doc.getElementsByClassName('slide')
+        for(let i = 0; i <= 1; i++) {
+            arrowL.classList.add('arrow-left')
+            arrowR.classList.add('arrow-right')
+            slide[0].appendChild(arrowL)
+            slide[0].appendChild(arrowR)
+        }
+    }
+
+
+
+    //FUNCTION CALL
     menu()
     slider()
 }
