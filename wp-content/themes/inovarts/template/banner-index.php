d69d3eebdb957dd5d00b8b2bdb7a6a979cd4ec06
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-13 20:46:53 -0300

    remove no usable tags

--- wp-content/themes/inovarts/template/banner-index.php
+++ wp-content/themes/inovarts/template/banner-index.php
@@ -1,9 +1,7 @@
 <div class="slider">
     <div class="slide">
         <div id="wrapper">
-            <img id="img-1" src="<?php bloginfo('template_url') ?>/assets/img/home.jpg" alt="img" />
-            <img id="img-2" src="<?php bloginfo('template_url') ?>/assets/img/night.jpg" alt="img" />
-            <img id="img-3" src="<?php bloginfo('template_url') ?>/assets/img/landscape.jpg" alt="img" />
+            <img src="<?php bloginfo('template_url') ?>/assets/img/home.jpg" alt="img" />
         </div>
         <div class="box row">
             <div class="col s12 m4 card-service-home">
