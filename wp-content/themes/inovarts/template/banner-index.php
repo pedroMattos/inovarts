e7bbef65fbae2176aeec44118bd779e51abc8a09
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-14 23:51:48 -0300

    add more tags

--- wp-content/themes/inovarts/template/banner-index.php
+++ wp-content/themes/inovarts/template/banner-index.php
@@ -2,19 +2,29 @@
     <div class="slide">
         <div id="wrapper">
             <img src="<?php bloginfo('template_url') ?>/assets/img/home.jpg" alt="img" />
+            <div class="cta-field">
+                <div id="text">
+                    <p>Lorem Ipsum</p>
+                    <h1>Lorem ipsum dolor sit amet </h1>
+                </div>
+            </div>
         </div>
+        <img id="logo" src="<?php bloginfo('template_url'); ?>/assets/img/logo-inovarts.png">
         <div class="box row">
             <div class="col s12 m4 card-service-home">
                 <?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/worldwide.svg') ?>
                 <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             <div class="col s12 m4 card-service-home">
                 <?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/social-media.svg') ?>
                 <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             <div class="col s12 m4 card-service-home">
                 <?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/ebook.svg') ?>
                 <h2>Lorem Ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
         </div>
     </div>
