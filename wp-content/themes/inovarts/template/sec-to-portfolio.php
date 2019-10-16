410d7f201bb96d388e084fb8a8a8675b6569e5f1
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-16 07:38:13 -0300

    add new section "sec-to-portfolio"

---
+++ wp-content/themes/inovarts/template/sec-to-portfolio.php
@@ -0,0 +1,10 @@
+<section class="row" id="to-portfolio">
+    <div class="col m9 s12" id="text-cta">
+        <div class="container" id="content">
+            <h2 class="white-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h2>
+            <div class="line row">
+                <div class="white col m8"></div><div class="grey-dark col m3"></div><div class="grey-ligh col m1"></div>
+            </div>
+        </div>
+    </div>
+</section>
