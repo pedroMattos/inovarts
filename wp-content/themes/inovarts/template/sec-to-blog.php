929106c797d45c44d45ec453aa9ce3dc6aeda12c
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-17 21:52:13 -0300

    add new section

---
+++ wp-content/themes/inovarts/template/sec-to-blog.php
@@ -0,0 +1,16 @@
+<section class="row" id="to-blog">
+    <div class="col m9 s12" id="header">
+        <div class="row">
+            <div class="col m6 s12">
+                <h2>See Our Blog</h2>
+            </div>
+            <div id="btn-blog" class="col m6 s12">
+                <a href="<?php bloginfo('url') ?>/blog"><button class="btn-cta">Go to</button></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row container" id="card-area">
+        <?php include 'cards-blog.php'; ?>
+    </div>
+</section>
