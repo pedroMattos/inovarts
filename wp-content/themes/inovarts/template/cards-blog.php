36a42d3a417a46529b4fe5b171ab81cdce6f18fa
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-17 21:52:04 -0300

    add new card

---
+++ wp-content/themes/inovarts/template/cards-blog.php
@@ -0,0 +1,19 @@
+<div class="col m4 s12">
+    <div class="own-card">
+        <img src="<?php bloginfo('template_url') ?>/assets/img/landscape.jpg" />
+        <div class="category">
+            <div class="catgory-icon">
+            <?php echo file_get_contents("wp-content/themes/inovarts/assets/svg/cat_exemple.svg"); ?>
+            </div>
+            <div class="head-internal">
+
+            </div>
+            <div class="footer-internal">
+
+            </div>
+        </div>
+        <h2>Category</h2>
+        <p>Lorem Ipsum Dolor Sit Amet</p>
+        <p>by: <strong class="link-card">author_name</strong></p>
+    </div>
+</div>
