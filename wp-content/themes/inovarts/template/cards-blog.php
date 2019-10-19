79b5af9db24aecbc5f1e36e3e8e342e87e97b208
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-18 21:28:12 -0300

    add contents

--- wp-content/themes/inovarts/template/cards-blog.php
+++ wp-content/themes/inovarts/template/cards-blog.php
@@ -2,14 +2,15 @@
     <div class="own-card">
         <img src="<?php bloginfo('template_url') ?>/assets/img/landscape.jpg" />
         <div class="category">
-            <div class="catgory-icon">
+            <div class="category-icon">
             <?php echo file_get_contents("wp-content/themes/inovarts/assets/svg/cat_exemple.svg"); ?>
             </div>
             <div class="head-internal">
-
+                <h2>Category</h2>
             </div>
             <div class="footer-internal">
-
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>by: <strong class="link-card"><a href="">author_name</a></strong></p>
             </div>
         </div>
         <h2>Category</h2>
