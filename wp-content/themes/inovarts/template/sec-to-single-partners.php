9376757eb0e95e320147f94ab5c8fe9d7717617c
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-20 23:15:25 -0300

    add ection structure

---
+++ wp-content/themes/inovarts/template/sec-to-single-partners.php
@@ -0,0 +1,15 @@
+<section class="row" id="to-partners">
+    <div class="col m9 s12" id="header">
+        <div class="row">
+            <div class="col m6 s12">
+                <h2>See Our Partners</h2>
+            </div>
+            <div id="btn-blog" class="col m6 s12">
+                <a href="<?php bloginfo('url') ?>/partners"><button class="btn-cta">Go to</button></a>
+            </div>
+        </div>
+    </div>
+    <div class="row container" id="card-area-partners">
+        <?php include 'card-partners.php'; ?>
+    </div>
+</section>
