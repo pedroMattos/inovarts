e3c8c2a0f84ba364ac9561845cbdb7ea95c843e2
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-20 23:13:38 -0300

    import new section

--- wp-content/themes/inovarts/index.php
+++ wp-content/themes/inovarts/index.php
@@ -5,5 +5,6 @@
     get_template_part('/template/banner-index');
     get_template_part('/template/sec-to-portfolio');
     get_template_part('/template/sec-to-blog');
+    get_template_part('/template/sec-to-single-partners');
 
     get_footer();
