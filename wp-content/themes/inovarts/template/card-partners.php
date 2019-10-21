4a2bf60118336c5a6ccf8609c34024d192da4a62
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-20 23:15:15 -0300

    add card structure

---
+++ wp-content/themes/inovarts/template/card-partners.php
@@ -0,0 +1,21 @@
+<div class="row" id="partner-card">
+    <div id="left-content" class="col m6 s12">
+        <h2>Some of our clients</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <div class="center" id="button-area-partners">
+            <a href="<?php the_permalink() ?>"><button class="btn-cta">See this Job</button></a>
+        </div>
+    </div>
+    <div id="right-content" class="col m6 s12">
+        <div id="header-img">
+            <p>Company Name</p>
+            <img src="<?php bloginfo('template_url') ?>/assets/img/lotusx1.png" />
+        </div>
+        <img src="<?php bloginfo('template_url') ?>/assets/img/landscape.jpg" />
+        <div id="position-slide">
+            <p>.06</p>
+        </div>
+    </div>
+</div>
