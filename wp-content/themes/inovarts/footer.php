63cc76a23d1b048396a044b2152a94cf88a88222
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-22 06:15:32 -0300

    add footer structure

--- wp-content/themes/inovarts/footer.php
+++ wp-content/themes/inovarts/footer.php
@@ -1,5 +1,37 @@
     <!-- Footer -->
         <footer>
+            <div id="wrapp-footer">
+                <div class="row" id="body-footer">
+                    <div id="logo-footer" class="col s12 m3">
+                        <img id="logo-footer-img" src="<?php bloginfo('template_url'); ?>/assets/img/logo-inovarts.png">
+                    </div>
+                    <div class="col s12 m3">
+
+                    </div>
+                    <div class="col s12 m3">
+
+                    </div>
+                    <div class="col s12 m3">
+
+                    </div>
+                </div>
+                <div class="row" id="site-map">
+                    <div id="list" class="col s12 m4">
+                        <p class="inline">Site Map: </p>
+                        <ul class="inline">
+                            <li><a href="#" class="link_footer">link</a></li>
+                            <li><a href="#" class="link_footer">link</a></li>
+                            <li><a href="#" class="link_footer">link</a></li>
+                            <li><a href="#" class="link_footer">link</a></li>
+                            <li><a href="#" class="link_footer">link</a></li>
+                            <li><a href="#" class="link_footer">link</a></li>
+                        </ul>
+                    </div>
+                    <div id="copyright" class="col s12 m4">
+                        <p>Copyright © <?php echo date('Y') ?> INOVARTS STUDIOS. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
             <!-- Scripts jquery -->
             <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
             <!-- Scripts -->
