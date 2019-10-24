a4401ecc6b97f54125ddd48db5a2708272927c87
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-24 06:45:18 -0300

    add search e social-media structure

--- wp-content/themes/inovarts/header.php
+++ wp-content/themes/inovarts/header.php
@@ -14,7 +14,7 @@
         <!-- Main Style -->
         <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/main.css" type="text/css">
         <!-- FavIcon -->
-        <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/assets/img/favicon.png">
+        <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/assets/img/newfavicon.png">
         <title>Inovarts Studios</title>
         <?php wp_head(); ?>
     </head>
@@ -25,5 +25,44 @@
             <div class="line-menu" id="line-menu-1"></div>
             <div class="line-menu" id="line-menu-2"></div>
             <div class="line-menu" id="line-menu-3"></div>
+        </div>
+    </div>
+
+    <!-- Floater icon -->
+    <div id="floater-tools">
+        <div id="logo-on-mobile">
+            <a href="<?php bloginfo('url') ?>">
+                <img id="logo" src="<?php bloginfo('template_url'); ?>/assets/img/logo-inovarts.png">
+            </a>
+        </div>
+        <div id="search-in-site">
+            <div class="inline">
+                <?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/search.svg') ?>
+            </div>
+            <div class="inline">
+                <form method="POST" id="s-form">
+                    <input type="text" id="search-site" name="s" placeholder="Pesquisar no site" />
+                </form>
+            </div>
+        </div>
+        <div id="social-media">
+            <div class="inline">
+                <?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/avatar.svg') ?>
+            </div>
+            <div class="inline">
+                <a target="_blank" id="mail_to" href="mailto:email"><?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/envelope.svg') ?></a>
+            </div>
+            <div class="inline">
+                <a target="_blank" href="facebook.com"><?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/facebook.svg') ?></a>
+            </div>
+            <div class="inline">
+                <a target="_blank" href="instagram.com"><?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/instagram.svg') ?></a>
+            </div>
+            <div class="inline">
+                <a target="_blank" href="contato"><?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/linkedin.svg') ?></a>
+            </div>
+            <div class="inline">
+                <a id="chat_whatsapp" target="_blank" href="https://api.whatsapp.com/send?l=pt_br&phone=12345678"><?php echo file_get_contents('wp-content/themes/inovarts/assets/svg/whatsapp.svg') ?></a>
+            </div>
         </div>
     </div>
