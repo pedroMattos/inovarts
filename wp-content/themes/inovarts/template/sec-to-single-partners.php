e37185310513caea003eed4bcc3748b92f368c38
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-26 12:50:46 -0300

    change structure on section partners

--- wp-content/themes/inovarts/template/sec-to-single-partners.php
+++ wp-content/themes/inovarts/template/sec-to-single-partners.php
@@ -1,14 +1,5 @@
 <section class="row" id="to-partners">
-    <div class="col m9 s12" id="header">
-        <div class="row">
-            <div class="col m6 s12">
-                <h2>See Our Partners</h2>
-            </div>
-            <div id="btn-blog" class="col m6 s12">
-                <a href="<?php bloginfo('url') ?>/partners"><button class="btn-cta">Go to</button></a>
-            </div>
-        </div>
-    </div>
+    <h2 class="center">Our Partners</h2>
     <div class="row container" id="card-area-partners">
         <?php include 'card-partners.php'; ?>
     </div>
