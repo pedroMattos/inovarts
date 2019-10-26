7171f504b4de903067191a394af3e1e575c87514
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-26 12:50:31 -0300

    add section structure header-pages

---
+++ wp-content/themes/inovarts/template/header-page.php
@@ -0,0 +1,7 @@
+<section class="hd-pages">
+    <?php the_post_thumbnail('medium_large') ?>
+    <div id="title">
+        <h1>Contact</h1>
+        <p><a href="<?php bloginfo('url') ?>">Home</a> >> <?php the_title(); ?></p>
+    </div>
+</section>
