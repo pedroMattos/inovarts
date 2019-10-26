e13613988b492689907856a73212ee261a9250e1
Pedro Mattos <pedro@dnadevendas.com.br>
2019-10-26 12:48:48 -0300

    add more comands to function menu

--- wp-content/themes/inovarts/assets/js/main.js
+++ wp-content/themes/inovarts/assets/js/main.js
@@ -8,20 +8,42 @@ window.onload = function() {
         var lines = doc.getElementsByClassName('line-menu')
         var close = doc.createElement('span')
         var wrapper = doc.getElementById('lines')
+        var content = doc.getElementById('content-text');
+        var item = doc.getElementsByClassName('item')
         close.textContent = 'x'
         wrapper.onclick = function() {
+            content.style.display = 'block';
             close.classList.add('close-menu')
             menu.classList.add('expanded-menu')
             menu.appendChild(close)
             close.classList.add('show-close-btn')
             wrapper.setAttribute('style', 'display:none')
+            setTimeout(function() {
+                try{
+                    for(var i = 0; i <= item.length; i++) {
+                        item[i].setAttribute('style', 'transform: translateY(-45%); transition-duration: .5s;')
+                    }
+                } catch(e) {
+                    console.warn(e);
+                }
+            }, 700)
         }
         close.onclick = function() {
-            if(menu.classList.contains('expanded-menu')) {
-                menu.classList.remove('expanded-menu')
+            try{
+                for(var i = 0; i <= item.length; i++) {
+                    item[i].removeAttribute('style')
+                }
+            } catch(e) {
+                console.warn(e);
             }
-            menu.removeChild(menu.children[1])
-            wrapper.removeAttribute('style','display:none')
+            setTimeout(function() {
+                if(menu.classList.contains('expanded-menu')) {
+                    menu.classList.remove('expanded-menu')
+                }
+                content.style.display = 'none';
+                menu.removeChild(menu.children[2])
+                wrapper.removeAttribute('style','display:none')
+            }, 600)
         }
     }
 
